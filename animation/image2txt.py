cdc9cc80dac3264a3f2e2ad5d929dd30efb4898d
Hitsme <1182681022@qq.com>
2018-08-22 21:58:06 +0800

    2

--- animation/image2txt.py
+++ animation/image2txt.py
@@ -4,22 +4,20 @@ import os
 from PIL import Image
 import numpy
 def image_to_txt(image_path, txt_path):
-    txt_count = 1                                   # 用于命名txt文件
-    fileList = os.listdir(image_path)               # 返回所有图片名称，是个字符串列表
-    for file in fileList:                           # 遍历每一张图片
+    txt_count = 1
+    fileList = os.listdir(image_path)
+    for file in fileList:
         img = Image.open(image_path + '\\'+ file).convert('L')
-        # 这里使用到PIL库convert函数，将RGB图片转化为灰度图，参数'L'代表转化为灰度图
         charWidth = 140
-        # 这个是设置你后面在cmd里面显示内容的窗口大小，请根据自己的情况，适当调整值
         img = img.resize((charWidth, 40))
         target_width, target_height = img.size
         data = numpy.array(img)[:target_height, :target_width]
         # 使用numpy库，将图像转化为数组
         with open(txt_path + '\\' + str(txt_count) + '.txt', 'w', encoding='utf-8') as f:
-            txt_count += 1                      # 一张图对应一个txt文件，所以每遍历一张图，该值加一
+            txt_count += 1
             for row in data:
                 for pixel in row:
-                    if pixel < 127:             # 如果灰度值小于127，也就是偏黑的，就写一个字符 '*'
+                    if pixel < 127:
                         f.write('*')
                     else:
                         f.write(' ')
--- animation/running.py
+++ animation/running.py
@@ -5,13 +5,10 @@ from animation import image2txt
 from animation import video2image
 def run(txt_path):
     fileList = os.listdir(txt_path)
-    for i in range(1, len(fileList)+1):         # 遍历所有的txt文件
+    for i in range(1, len(fileList)+1):
         try:
             os.system('type ' + txt_path + '\\' + str(i) + '.txt')
-            # 这里type命令是Windows下的命令，相信很多人没怎么用过，你试一下就知道，type+文件名，就可以在cmd里面显示文件内容
             os.system('cls')
-            # 清屏的意思，每显示一次txt文件，就清屏，然后显示下一个txt
-            # 这里还可以适当的加延时函数，如果显示的太快的话
         except:
             print('ERROR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
